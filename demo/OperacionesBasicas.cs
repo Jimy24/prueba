a2382fac8c77e50d2c2fe8fe58f964f72ed8369e
jifeve24 <jifeve24@gmail.com>
2019-12-04 23:02:18 -0600

    test

---
+++ demo/OperacionesBasicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo
+{
+    public class OperacionesBasicas
+
+    {
+
+        public decimal suma( Decimal numero1, Decimal numero2)
+        {
+            return numero1 + numero2;
+        }
+        public decimal resta(Decimal numero1, Decimal numero2)
+        {
+            return numero1 -numero2;
+        }
+        public decimal multiplicacion(Decimal numero1, Decimal numero2)
+        {
+            return numero1 * numero2;
+        }
+        public decimal division(Decimal numero1, Decimal numero2)
+        {
+            return numero1 / numero2;
+        }
+    }
+}
---
+++ demo/OperacionesBasicas_Test.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo
+{
+    [TestFixture]
+    public class OperacionesBasicas_Test
+    {
+        [Test]
+        public void  Sumar_SumaCorrectamente(){
+            //Arrenge
+            decimal numero1 = 2;
+            decimal numero2 = 5;
+           
+
+            OperacionesBasicas ob = new OperacionesBasicas();
+            // ACT
+            decimal resultado =ob.suma(numero1, numero2);
+            //Assert
+
+            Assert.AreEqual(7, resultado);
+        }
+
+        [Test]
+        public void Resta_RestaMayorCero()
+        {
+            //Arrenge
+            decimal numero1 = 1;
+            decimal numero2 = 5;
+
+
+            OperacionesBasicas ob = new OperacionesBasicas();
+            // ACT
+            decimal resultado = ob.resta(numero1, numero2);
+            //Assert
+
+            Assert.LessOrEqual(0, resultado);
+        }
+
+
+        [Test]
+        public void Division_DivisorMayorCero()
+        {
+            //Arrenge
+            decimal numero1 = 0;
+            decimal numero2 = 0;
+
+
+            OperacionesBasicas ob = new OperacionesBasicas();
+            // ACT
+            //decimal resultado = ob.resta(numero1, numero2);
+            //Assert
+
+            Assert.Throws<DivideByZeroException>(new TestDelegate(() => ob.division(numero1, numero2)));
+        }
+    }
+}
